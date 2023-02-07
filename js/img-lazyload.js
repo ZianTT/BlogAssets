0f32e01bad3ae5555b0508a997ebf6c86be475f1
ZianTT <2508164094@qq.com>
2023-02-07 17:01:46 +0800

    1.0.6

--- js/img-lazyload.js
+++ js/img-lazyload.js
@@ -1,10 +1,1 @@
-/* global ZianTT, CONFIG */
-
-(function(window, document) {
-  for (const each of document.querySelectorAll('img[lazyload]')) {
-    ZianTT.utils.waitElementVisible(each, function() {
-      each.removeAttribute('srcset');
-      each.removeAttribute('lazyload');
-    }, CONFIG.lazyload.offset_factor);
-  }
-})(window, document);
+!function(){for(const t of document.querySelectorAll("img[lazyload]"))ZianTT.utils.waitElementVisible(t,function(){t.removeAttribute("srcset"),t.removeAttribute("lazyload")},CONFIG.lazyload.offset_factor)}(window);
